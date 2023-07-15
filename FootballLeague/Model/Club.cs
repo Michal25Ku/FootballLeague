69dd1c0e3c92d339d6d3c0ab48795442a0459fcf
Michał Kuciak <michal.kuciak@microsoft.wsei.edu.pl>
2023-07-15 19:58:54 +0200

    Połączenie z bazą danych i mapowanie tabeli Clubs

---
+++ FootballLeague/Model/Club.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballLeagueLib.Model
+{
+    public class Club
+    {
+        #region mapowanie kolumn
+        [Key]
+        [Column("IdClub")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdClub { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ClubName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string StadiumName { get; set; }
+
+        public int GoalsScored { get; set; }
+        public int GoalsConceded { get; set; }
+        public int GoalBalance { get; set; }
+        public int Wins { get; set; }
+        public int Draws { get; set; }
+        public int Failures { get; set; }
+        #endregion
+
+        public virtual ICollection<Player> Players { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Match> MatchesGuest { get; set; }
+        [NotMapped]
+        public virtual ICollection<Match> MatchesHost { get; set; }
+
+        public Club()
+        {
+            GoalsScored = 0;
+            GoalsConceded = 0;
+            GoalBalance = 0;
+            Wins = 0;
+            Draws = 0;
+            Failures = 0;
+
+            this.Players = new HashSet<Player>();
+            this.MatchesGuest = new HashSet<Match>();
+            this.MatchesHost = new HashSet<Match>();
+        }
+
+    }
+}
---
+++ FootballLeague/Model/FootballLeague.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballLeagueLib.Model
+{
+    public class FootballLeague : DbContext
+    {
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Goal> Goals { get; set; }
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<Player> Players { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, "FootballLeague.mdf");
+            optionsBuilder.UseSqlServer($"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={path};Integrated Security=True");
+        }
+    }
+}
