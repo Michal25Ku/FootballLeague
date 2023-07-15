8cf6818b986597b68aa2822dc08e5d41b5f934e5
Michał Kuciak <michal.kuciak@microsoft.wsei.edu.pl>
2023-07-15 20:01:19 +0200

    Mapowanie tabeli Goals

--- FootballLeague/Model/Goal.cs
+++ FootballLeague/Model/Goal.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,29 @@ namespace FootballLeagueLib.Model
 {
     public class Goal
     {
+        #region mapowanie kolumn
+        [Key]
+        [Column("IdGoal")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdGoal { get; set; }
 
+        public int MinuteOfTheMatch { get; set; }
+        #endregion
+
+        #region Foreign key
+        [ForeignKey("Player")]
+        public int IdPlayer { get; set; }
+        public virtual Player Player { get; set; }
+
+        [ForeignKey("Club")]
+        public int IdClub { get; set; }
+        public virtual Club Club { get; set; }
+
+        [ForeignKey("Match")]
+        public int IdMatch { get; set; }
+        public virtual Match Match { get; set; }
+        #endregion
+
+        public Goal() { }
     }
 }
