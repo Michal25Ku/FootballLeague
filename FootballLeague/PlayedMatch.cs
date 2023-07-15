7084b9eaa3a45e105f843dcd4f706de20285d04d
Michał Kuciak <michal.kuciak@microsoft.wsei.edu.pl>
2023-07-16 01:10:28 +0200

    Utworzenie metody ShootGoal()

--- FootballLeague/PlayedMatch.cs
+++ FootballLeague/PlayedMatch.cs
@@ -10,7 +10,7 @@ namespace FootballLeagueLib
 {
     public class PlayedMatch
     {
-        public int IdMatch { get; }
+        public int IdMatch { get; private set; }
         public int IdHomeTeam { get; }
         public int IdAwayTeam { get; }
 
@@ -32,9 +32,10 @@ namespace FootballLeagueLib
             IdHomeTeam = idHomeTeam;
             IdAwayTeam = idAwayTeam;
             PlayMatch(idHomeTeam, idAwayTeam, matchDate);
+            IdMatch = db.Matches.OrderBy(m => m.IdMatch).Select(m => m.IdMatch).FirstOrDefault();
         }
 
-        bool PlayMatch(int idHomeTeam, int idAwayTeam, DateTime matchDate)
+        private bool PlayMatch(int idHomeTeam, int idAwayTeam, DateTime matchDate)
         {
             using var db = new FootballLeague();
 
@@ -50,7 +51,33 @@ namespace FootballLeagueLib
             };
 
             db.Matches.Add(newMatch);
+            return SaveChange(db);
+        }
 
+        public bool ShootGoal(int minuteOfMatch, int idClub, int idPlayer)
+        {
+            if (minuteOfMatch < 1 || minuteOfMatch > 90)
+                throw new ArgumentOutOfRangeException("Podano nieprawidłową minutę meczu!");
+            if (idClub != IdHomeTeam && idClub != IdAwayTeam)
+                throw new ArgumentException("Podana drużyna nie gra w tym meczu!");
+
+            using var db = new FootballLeague();
+
+            if (db.Players.FirstOrDefault(p => p.IdPlayer == idPlayer).IdClub != IdHomeTeam && db.Players.FirstOrDefault(p => p.IdPlayer == idPlayer).IdClub != IdAwayTeam)
+                throw new ArgumentException("Podany zawodnik nie gra w tym meczu!");
+
+            if (db.Players.FirstOrDefault(p => p.IdPlayer == idPlayer).IdClub != idClub)
+                throw new ArgumentException("Podany zawodnik nie gra dla tego klubu!");
+
+            var newGoal = new Goal
+            {
+                MinuteOfTheMatch = minuteOfMatch,
+                IdPlayer = idPlayer,
+                IdClub = idClub,
+                IdMatch = this.IdMatch
+            };
+
+            db.Goals.Add(newGoal);
             return SaveChange(db);
         }
 
