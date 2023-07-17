d952a0638ebb0caa173d1dbcde823de7d1bce22b
Michał Kuciak <michal.kuciak@microsoft.wsei.edu.pl>
2023-07-17 22:06:41 +0200

    generowanie meczy

--- TestConsoleAplication/Program.cs
+++ TestConsoleAplication/Program.cs
@@ -6,11 +6,8 @@ namespace TestConsoleAplication
     {
         static void Main(string[] args)
         {
-            PlayedMatch nowyMecz = new PlayedMatch(1, 2);
-            PlayedMatch nowyMecz1 = new PlayedMatch(1, 2);
-
-            MatchTracking rozegrajMecz = new MatchTracking(50, nowyMecz1);
-            rozegrajMecz.StartMatch();
+            Season seaon1 = new Season();
+            //rozegrajMecz.StartMatch();
 
         }
     }
