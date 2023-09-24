a6585dfed600c2a3aa72ecf32b7f7776ce7b7c2b
Michał Kuciak <michal.kuciak@microsoft.wsei.edu.pl>
2023-09-24 22:21:48 +0200

    The GetPlayersFromTeam method in the MatchPlater class now uses IQueryable<Player> to quickly take data about players in the club.

--- FootballLeague/PlayMatch/MatchPlayers.cs
+++ FootballLeague/PlayMatch/MatchPlayers.cs
@@ -12,17 +12,9 @@ namespace FootballLeagueLib.PlayMatch
     {
         public List<Player> GetPlayersFromTeam(int idClub)
         {
-            Random rand = new Random(); // simulation, which players will be playing
             using var db = new FootballLeagueContext();
-            List<Player> players = db.Players.Where(p => p.ClubId == idClub).ToList();
-
-            while (players.Count > 11)
-            {
-                if (players[rand.Next(players.Count)] is not null)
-                    players.Remove(players[rand.Next(players.Count)]);
-            }
-
-            return players;
+            IQueryable<Player> players = db.Players.Where(p => p.ClubId == idClub);
+            return players.ToList();
         }
     }
 }
