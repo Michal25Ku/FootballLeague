3630339831bbe9849f4ff97acf6cd5e29f4b949a
Michał Kuciak <michal.kuciak@microsoft.wsei.edu.pl>
2023-09-28 23:19:37 +0200

    Documentation2

--- FootballLeague/PlayMatch/MatchManager.cs
+++ FootballLeague/PlayMatch/MatchManager.cs
@@ -1,45 +1,84 @@
 ﻿using FootballLeagueLib.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using FootballLeagueLib.Interfaces;
-using System.Text.RegularExpressions;
 using Match = FootballLeagueLib.Entities.Match;
-using FootballLeagueLib.Table;
-using FootballLeagueLib.Season;
 
 namespace FootballLeagueLib.PlayMatch
 {
+    /// <summary>
+    /// This delegate responsibilities for called methods in the WPF application which changes a displayed match result
+    /// </summary>
+    /// <param name="minuteOfMatch">minute when a goal is scored</param>
+    /// <param name="player">player who scored a goal</param>
+    /// <param name="isHomeTeamShotGoal">flag which check is a home team(true) or an away team(false) scored a goal</param>
     public delegate void MatchResultChangedHandler(int minuteOfMatch, Player player, bool isHomeTeamShotGoal);
+
+    /// <summary>
+    /// This delegate responsibilities for changed a time of match in each minute
+    /// </summary>
     public delegate void MatchTimeChangedHandler();
+
+    /// <summary>
+    /// This delegate responsibilities for the execution of all task in a WPF application related to match and table
+    /// </summary>
     public delegate void MatchEndChangedHandler();
+
+    /// <summary>
+    /// This delegate responsibilities for starting the match 
+    /// </summary>
     public delegate void MatchStartChangedHandler();
     public class MatchManager
     {
         #region events
+        /// <summary>
+        /// An event is called when a goal is scored
+        /// </summary>
         public event MatchResultChangedHandler MatchResultChanged;
+
+        /// <summary>
+        /// An event is called in each minute of the match
+        /// </summary>
         public event MatchTimeChangedHandler MatchTimeChanged;
+
+        /// <summary>
+        /// An event is called when match is gone
+        /// </summary>
         public event MatchEndChangedHandler MatchEndChanged;
+
+        /// <summary>
+        /// An event is called when the match is started
+        /// </summary>
         public event MatchStartChangedHandler MatchStartChanged;
         #endregion
 
+        /// <summary>
+        /// Match count 90 minutes, this is a fact
+        /// </summary>
         public const int MATCH_TIME = 90;
         List<int> _playerWeightNumbers;
-        // TODO 
-        // need to create separate maybe a progress bar in the menu. we could change this value if we want the match to be played faster or slower
+
+        /// <summary>
+        /// The speed of simulation in real time
+        /// TODO
+        /// Bar which changes a simulation time
+        /// </summary>
         public int SimulationTime { get; }
         public int MinuteInMatch { get; private set; }
 
         public Match PlayedMatch { get; private set; }
+
+        /// <summary>
+        /// This class has scored goal logic
+        /// </summary>
         public MatchScoredGoal MatchScoreGoal { get; }
         public MatchPlayers MatchPlayers { get; }
 
         public List<Player> HomeTeamPlayers { get; private set; }
         public List<Player> AwayTeamPlayers { get; private set; }
 
+
+        /// <summary>
+        /// Constructor initializes the HomeTeamPlayers and the AwayTeamPlayers lists and sets the MinuteInMatch to 0
+        /// </summary>
+        /// <param name="playedMatch">The match during which all tasks related to its playing will be performed</param>
         public MatchManager(Match playedMatch)
         {
             MatchScoreGoal = new MatchScoredGoal();
@@ -48,12 +87,22 @@ namespace FootballLeagueLib.PlayMatch
 
             HomeTeamPlayers = MatchPlayers.GetPlayersFromTeam(playedMatch.HomeTeamId);
             AwayTeamPlayers = MatchPlayers.GetPlayersFromTeam(playedMatch.AwayTeamId);
+
+            //It creates the weight numbers list for the simulation random selection player
             _playerWeightNumbers = GenerateWeightedNumbers();
 
             SimulationTime = 0;
             MinuteInMatch = 0;
         }
 
+        /// <summary>
+        /// Starts the match and counts down time from 0 to 90 minutes.
+        /// On start invokes the MatchStartChanged event.
+        /// Initializes values for the match, sets the result to "0 - 0", and IsPlayed sets to true.
+        /// It simules when and who score a goal.
+        /// On the end, invokes the MatchEndChanged event
+        /// </summary>
+        /// <returns></returns>
         public async Task StartMatch()
         {
             MatchStartChanged?.Invoke();
@@ -65,19 +114,28 @@ namespace FootballLeagueLib.PlayMatch
 
             for (int i = 1; i <= MATCH_TIME + 1; i++)
             {
+                /// Each minute invokes this event to change the time in the match view
                 MatchTimeChanged?.Invoke();
+
+                // 1% chance to score goal
                 if (rand.Next(100) <= 1)
                 {
+                    // each club has 50% chance to score the goal
                     int teamShoot = rand.Next(2);
+
                     if (teamShoot == 0)
                     {
+                        // The chance to score a goal by a specific player depends on player position in the playground
                         int playerShoot = _playerWeightNumbers[rand.Next(_playerWeightNumbers.Count - 1)];
+
                         MatchScoreGoal.ScoreGoal(i, PlayedMatch.HomeTeamId, HomeTeamPlayers[playerShoot - 1].IdPlayer, this);
                         MatchResultChanged?.Invoke(i, HomeTeamPlayers[playerShoot - 1], true);
                     }
                     else
                     {
+                        // The chance to score a goal by a specific player depends on player position in the playground
                         int playerShoot = _playerWeightNumbers[rand.Next(_playerWeightNumbers.Count - 1)];
+
                         MatchScoreGoal.ScoreGoal(i, PlayedMatch.AwayTeamId, AwayTeamPlayers[playerShoot - 1].IdPlayer, this);
                         MatchResultChanged?.Invoke(i, AwayTeamPlayers[playerShoot - 1], false);
                     }
@@ -93,7 +151,11 @@ namespace FootballLeagueLib.PlayMatch
             MatchEndChanged?.Invoke();
         }
 
-        List<int> GenerateWeightedNumbers()
+        /// <summary>
+        /// Generates numbers depends on player position to simulation which player is scored a goal
+        /// </summary>
+        /// <returns>List numbers like '1, 2, 2, 3, 3, 3, ...'</returns>
+        public List<int> GenerateWeightedNumbers()
         {
             List<int> weightedNumbers = new List<int>();
 
